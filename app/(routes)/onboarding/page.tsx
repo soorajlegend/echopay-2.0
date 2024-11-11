93a5fc9fce09df4f7d100ba64d91da83c1c88e03
REF <REF@kfnl.gov.sa>
2024-11-11 16:21:38 +0300

    added the onboarding page

---
+++ app/(routes)/onboarding/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const OnboardingPage = () => {
+  return (
+    <div className="w-full h-full flex flex-col gap-10">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-lg font-semibold">Join the Future of Finance</h1>
+        <p className="text-base">
+          Create your EchoPay account today and experience AI powered financial
+          transactions
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default OnboardingPage;
