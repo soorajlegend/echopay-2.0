bc1cc000118a47c2f5c101b91e84a7bb2c49d9e0
amjido-01 <youndsadeeq10@gmail.com>
2024-11-11 05:32:53 -0800

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
-import { Button } from "@/components/ui/button";
+import logo from "../assets/e.svg"
+import Image from "next/image";
 export default function Home() {
   return (
-    <div>
-      <Button>Clik me</Button>
+    <div className=" bg-background flex justify-center items-center max-w-lg mx-auto border-2 h-screen">
+      <Image src={logo}
+        width={65}
+        height={13}
+        alt="Picture of the author"
+      />
     </div>
   );
 }
