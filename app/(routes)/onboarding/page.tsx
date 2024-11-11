5ee9f4513b84f7ae6fb06a3751fa978c71b6e95e
amjido-01 <youndsadeeq10@gmail.com>
2024-11-11 06:20:21 -0800

    components

--- app/(routes)/onboarding/page.tsx
+++ app/(routes)/onboarding/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const OnboardingPage = () => {
   return (
-    <div className="w-full h-full flex flex-col gap-10">
+    <div className="w-full h-full border-2 flex flex-col gap-10">
       <div className="flex flex-col gap-2">
         <h1 className="text-lg font-semibold">Join the Future of Finance</h1>
         <p className="text-base">
