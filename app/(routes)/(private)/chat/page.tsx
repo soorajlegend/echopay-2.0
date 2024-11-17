247a91a30494f3a4f5477b88d977b6d11a2f916a
Suraj <soorajwizard01@gmail.com>
2024-11-17 10:48:14 +0300

    added voice page

--- app/(routes)/(private)/chat/page.tsx
+++ app/(routes)/(private)/chat/page.tsx
@@ -147,7 +147,7 @@ const ChatPage = () => {
   return (
     <div className="relative flex flex-col w-full h-screen p-4 pt-0">
       <div className="flex items-center justify-between sticky top-0 bg-white px-4 py-2">
-        <Link href="/dashboard" className="flex items-center gap-2">
+        <Link href="/dashboard" className="flex items-center">
           <ChevronLeft className="w-10 h-10 p-1.5" />
           <h2 className="text-xl font-medium">Chat</h2>
         </Link>
