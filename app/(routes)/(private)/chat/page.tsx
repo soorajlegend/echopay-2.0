57d1cd787b99188f965adf4b96362331655b2236
Suraj <soorajwizard01@gmail.com>
2024-11-18 15:02:56 +0300

    added transcriber

--- app/(routes)/(private)/chat/page.tsx
+++ app/(routes)/(private)/chat/page.tsx
@@ -133,7 +133,7 @@ const ChatPage = () => {
 
   return (
     <div className="relative flex flex-col w-full h-screen p-4 pt-0">
-      <div className="flex items-center justify-between sticky top-0 bg-white px-4 py-2">
+      <div className="flex items-center justify-between sticky top-0 z-50 bg-white px-4 py-2">
         <Link href="/dashboard" className="flex items-center">
           <ChevronLeft className="w-10 h-10 p-1.5" />
           <h2 className="text-base lg:text-lg font-semibold">Chat</h2>
