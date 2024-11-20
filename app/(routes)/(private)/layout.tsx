87096fad818cafc5fd1f0edcfb5a347ae40f320e
Suraj <soorajwizard01@gmail.com>
2024-11-20 14:02:40 +0300

    added book keeping feature

--- app/(routes)/(private)/layout.tsx
+++ app/(routes)/(private)/layout.tsx
@@ -26,7 +26,8 @@ export default function RoutesLayout({
       />
 
       {openEcho && <Echo />}
-      {showChart && <Chart />}
+      {/* {showChart && <Chart />} */}
+      <Chart />
     </>
   );
 }
