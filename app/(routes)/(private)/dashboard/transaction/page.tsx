9d3d173a480b1e7c361d0d9e46b3ee588d3bd720
amjido-01 <youndsadeeq10@gmail.com>
2024-11-20 11:44:12 +0000

    dashbooard corrections

--- app/(routes)/(private)/dashboard/transaction/page.tsx
+++ app/(routes)/(private)/dashboard/transaction/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import React from 'react'
-import { Image } from 'lucide-react'
 import { MoveLeft, Bell } from 'lucide-react'
 import { useRouter } from 'next/navigation';
 import useUserInfo from '@/hooks/use-userinfo';
