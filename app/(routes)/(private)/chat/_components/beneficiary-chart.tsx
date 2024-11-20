2cceb2033ccd9828edb0b07e52abb0bd2bbeaa41
Suraj <soorajwizard01@gmail.com>
2024-11-20 15:46:43 +0300

    added book keeping feature

--- app/(routes)/(private)/chat/_components/beneficiary-chart.tsx
+++ app/(routes)/(private)/chat/_components/beneficiary-chart.tsx
@@ -44,7 +44,7 @@ export const BeneficiaryChart = () => {
   }, [transactions, filter]);
 
   return (
-    <div className="min-h-60 flex flex-col gap-y-4">
+    <div className="min-h-60 flex flex-col gap-y-4 py-4 px-2 lg:px-5">
       <div className="flex gap-2">
         <Button
           variant={filter === "all" ? "default" : "outline"}
