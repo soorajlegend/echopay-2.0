6abce31edeb1f899bb513627325c03782bb86549
amjido-01 <youndsadeeq10@gmail.com>
2024-11-21 05:45:11 +0000

    transfer page changes

---
+++ app/(routes)/(private)/transfer-echo/page.tsx
@@ -0,0 +1,106 @@
+"use client";
+import React from 'react'
+import { useState } from "react"
+import ConfirmTransaction from '@/components/ui/confirm-transaction';
+import TransactionInput from '@/components/ui/transaction-input';
+import SlideContainer from '@/components/slide-container';
+import axios from 'axios';
+
+
+const TransactionPage = () => {
+    const [selectedBank, setSelectedBank] = useState("");
+    const [accountNumber, setAccountNumber] = useState("");
+    const [amount, setAmount] = useState("");
+    const [stage, setStage] = useState(1);
+    const [pin, setPin] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [transactionStatus, setTransactionStatus] = useState("");
+
+ 
+    const handleConfirm = () => {
+        if (!selectedBank || !accountNumber || !amount) {
+          alert("Please fill in all fields.");
+          return;
+        }
+        setStage(2);
+      };
+
+      //"https://echo-pay.onrender.com/api/transfer",
+      const handlePinSubmit = async (inputPin: string) => {
+        if (inputPin.length !== 5) {
+          alert("PIN must be 5 digits.");
+          return;
+        }
+        setPin(inputPin);
+        setIsLoading(true);
+        setErrorMessage("");
+
+        const payload = {
+            sender: "YourSenderIdentifier", // actual sender info
+            receiver: accountNumber,
+            amount: parseFloat(amount),
+            narration: `Transfer to ${selectedBank}`,
+            pin: parseInt(inputPin, 10),
+          };
+    
+        // Call the API with the payload
+        try {
+            const response = await axios.post("https://echo-pay.onrender.com/api/transfer", payload);
+
+            if (response.data.success) {
+                setTransactionStatus("Transaction Successful!");
+                setStage(3);
+              } else {
+                setErrorMessage(response.data.message || "Transaction Failed.");
+                setIsLoading(false);
+              }
+        } catch (error) {
+            setErrorMessage("Transaction Failed.");
+            setIsLoading(false) ;
+        }
+      };
+  
+  return (
+    <div className='p-6 h-screen flex items-center justify-center'>
+        {/* stages === 1 */}
+        {stage === 1 && (
+          <SlideContainer custom={stage}>
+            <ConfirmTransaction
+              selectedBank={selectedBank}
+              accountNumber={accountNumber}
+              amount={amount}
+              onBankSelect={setSelectedBank}
+              onAccountChange={setAccountNumber}
+              onAmountChange={setAmount}
+              onContinue={handleConfirm}
+            />
+          </SlideContainer>
+        )}
+        {/* stages === 2 */}
+        {stage === 2 && (
+          <SlideContainer custom={stage}>
+            <TransactionInput
+              onContinue={handlePinSubmit}
+              value={pin}
+              setPin={setPin}
+              isLoading={isLoading}
+              error=""
+            />
+          </SlideContainer>
+        )}
+        {stage === 3 && (
+          <div className="text-center">
+            <h2 className="text-xl font-bold mb-4">
+              {isLoading ? "Processing..." : "Transaction Successful!"}
+            </h2>
+            <p>Amount: {amount}</p>
+            <p>Account Number: {accountNumber}</p>
+            <p>Bank: {selectedBank}</p>
+          </div>
+        )}
+        </div>
+  )
+}
+
+export default TransactionPage
--- app/(routes)/(private)/transfer/page.tsx
+++ app/(routes)/(private)/transfer/page.tsx
@@ -1,106 +1,79 @@
-"use client";
+"use client"
+import { MoveLeft, ChevronRight } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 import React from 'react'
-import { useState } from "react"
-import ConfirmTransaction from '@/components/ui/confirm-transaction';
-import TransactionInput from '@/components/ui/transaction-input';
-import SlideContainer from '@/components/slide-container';
-import axios from 'axios';
-
-
-const TransactionPage = () => {
-    const [selectedBank, setSelectedBank] = useState("");
-    const [accountNumber, setAccountNumber] = useState("");
-    const [amount, setAmount] = useState("");
-    const [stage, setStage] = useState(1);
-    const [pin, setPin] = useState("");
-    const [isLoading, setIsLoading] = useState(false);
-    const [errorMessage, setErrorMessage] = useState("");
-    const [transactionStatus, setTransactionStatus] = useState("");
-
- 
-    const handleConfirm = () => {
-        if (!selectedBank || !accountNumber || !amount) {
-          alert("Please fill in all fields.");
-          return;
-        }
-        setStage(2);
-      };
-
-      //"https://echo-pay.onrender.com/api/transfer",
-      const handlePinSubmit = async (inputPin: string) => {
-        if (inputPin.length !== 5) {
-          alert("PIN must be 5 digits.");
-          return;
-        }
-        setPin(inputPin);
-        setIsLoading(true);
-        setErrorMessage("");
-
-        const payload = {
-            sender: "YourSenderIdentifier", // actual sender info
-            receiver: accountNumber,
-            amount: parseFloat(amount),
-            narration: `Transfer to ${selectedBank}`,
-            pin: parseInt(inputPin, 10),
-          };
-    
-        // Call the API with the payload
-        try {
-            const response = await axios.post("https://echo-pay.onrender.com/api/transfer", payload);
-
-            if (response.data.success) {
-                setTransactionStatus("Transaction Successful!");
-                setStage(3);
-              } else {
-                setErrorMessage(response.data.message || "Transaction Failed.");
-                setIsLoading(false);
-              }
-        } catch (error) {
-            setErrorMessage("Transaction Failed.");
-            setIsLoading(false) ;
-        }
-      };
-  
+import Image from 'next/image'
+import useUserInfo from '@/hooks/use-userinfo'
+import Link from 'next/link'
+const Page = () => {
+    const {info} = useUserInfo()
+    const router = useRouter()
   return (
-    <div className='p-6 h-screen flex items-center justify-center'>
-        {/* stages === 1 */}
-        {stage === 1 && (
-          <SlideContainer custom={stage}>
-            <ConfirmTransaction
-              selectedBank={selectedBank}
-              accountNumber={accountNumber}
-              amount={amount}
-              onBankSelect={setSelectedBank}
-              onAccountChange={setAccountNumber}
-              onAmountChange={setAmount}
-              onContinue={handleConfirm}
-            />
-          </SlideContainer>
-        )}
-        {/* stages === 2 */}
-        {stage === 2 && (
-          <SlideContainer custom={stage}>
-            <TransactionInput
-              onContinue={handlePinSubmit}
-              value={pin}
-              setPin={setPin}
-              isLoading={isLoading}
-              error=""
-            />
-          </SlideContainer>
-        )}
-        {stage === 3 && (
-          <div className="text-center">
-            <h2 className="text-xl font-bold mb-4">
-              {isLoading ? "Processing..." : "Transaction Successful!"}
-            </h2>
-            <p>Amount: {amount}</p>
-            <p>Account Number: {accountNumber}</p>
-            <p>Bank: {selectedBank}</p>
-          </div>
-        )}
+    <div className='overflow-y-auto h-full p-6 justify-between mb-10'>
+        <div>
+        <div className="flex gap-[100px] items-center">
+      <MoveLeft onClick={() => router.back()} className="w-[24px] h-[24px] cursor-pointer text-[#003056]" />
+    <h2>Transfer</h2>
+    </div>
+
+    <div className="h-[111px] w-full text-white mt-[60px] flex p-4 justify-between rounded-[8px] bg-[#003056]">
+            <div className="flex flex-col justify-between">
+              <p className="">Balance</p>
+              <p className="text-[22px] md:text-[24px] font-semibold">
+                N {info?.balance}
+              </p>
+            </div>
+            <div className="relative flex flex-col justify-end w-1/2">
+              <Image
+                className="absolute h-[80px] w-[74px] -top-4 -right-4"
+                src="/echo.svg"
+                width={0}
+                height={0}
+                alt="logo"
+              />
+              <div className="flex justify-end">
+                <Image
+                  className=" h-[20px] mb-1 w-[20px]"
+                  src="/eye.svg"
+                  width={20}
+                  height={14}
+                  alt="eye icon"
+                />
+              </div>
+            </div>
+    </div>
+
+    <div className='mt-[43px]'>
+        <h3 className='text-[#003056] text-[18px] leading-[24px] font-medium'>Select Transfer Option</h3>
+        <Link href="/transfer-echo">
+        <div className='flex border-2 mt-[16px] p-3  rounded-[8px] items-center gap-[30px]'>
+            <Image width={24} height={24} src="/transfer-echo.png" alt="logo" />
+            <div className='flex items-center w-[90%] justify-between'>
+                <div>
+                <p className='text-[#141B34] text-[16px] leading-[24px] font-medium'>Transfer to Echopay</p>
+                <span className='text-[#B0B0B0] text-[12px]'>Transfer to Echopay users</span>
+                </div>
+            <ChevronRight className='w-[24px] h-[24px] text-[#141B34]' />
+            </div>
         </div>
+        </Link>
+        
+
+        <div className='flex border-2 mt-[16px] p-3 cursor-pointer rounded-[8px] items-center gap-[30px]'>
+            <Image width={24} height={24} src="/bank.png" alt="logo" />
+            <div className='flex items-center w-[90%] justify-between'>
+                <div>
+                <p className='text-[#141B34] text-[16px] leading-[24px] font-medium'>Transfer to Bank</p>
+                <span className='text-[#B0B0B0] text-[12px]'>Transfer to other banks</span>
+                </div>
+            <ChevronRight className='w-[24px] h-[24px] text-[#141B34]' />
+            </div>
+        </div>
+    </div>
+
+        </div>
+    </div>
   )
 }
 
-export default TransactionPage
+export default Page
