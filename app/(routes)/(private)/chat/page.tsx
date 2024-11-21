48777e986edacf420a3ff9921d508a8eed0f8ba4
Suraj <soorajwizard01@gmail.com>
2024-11-21 14:20:46 +0300

    added more apis

--- app/(routes)/(private)/chat/page.tsx
+++ app/(routes)/(private)/chat/page.tsx
@@ -132,6 +132,8 @@ const ChatPage = () => {
         setNewTransaction(jsonData.newTransaction);
       }
 
+      console.log(jsonData);
+
       if (jsonData.message) {
         const modelMessage: Chat = {
           id: nanoid(),
